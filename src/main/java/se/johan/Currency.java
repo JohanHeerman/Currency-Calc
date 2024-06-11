24276999b2bef4b01c581b7067d521b5ab84338d
Johan Heerman <johanheerman@gmail.com>
2024-06-11 10:38:39 +0200

    first commit

---
+++ src/main/java/se/johan/Currency.java
@@ -0,0 +1,29 @@
+package se.johan;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Currency {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Currency Converter");
+    }
+
+     while (true) {
+
+        System.out.println("Menu");
+        System.out.println("1. SEK to USD");
+        System.out.println("2. USD to SEK");
+        System.out.println("3. SEK to EURO");
+        System.out.println("4. EURO to SEK");
+        System.out.println("5. Exit");
+
+        int choice = scanner.nextInt();
+
+
+    }
+
+
+}
